03a7e51b72601b82aa85817ccde0c21c52fd1dd8
f-s-minsky <f.s.minsky@gmail.com>
2022-04-26 16:01:37 +0200

    routing users et newspot pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
+import Users from './users/pages/Users';
+import NewSpot from './spots/pages/NewSpot';
+
 function App() {
-  return <h1>Hello !</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Users />
+        </Route>
+        <Route path='/spots/new'>
+          <NewSpot />
+        </Route>
+        <Redirect to='/' />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
